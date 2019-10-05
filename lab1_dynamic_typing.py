81481da2861113fe62d12a99c9bde48e07bdee06
Thomas Coll <thomascoll@Thomass-MacBook-Air.local>
2019-10-05 16:43:20 +0100

    bug is fixed

--- lab1_dynamic_typing.py
+++ lab1_dynamic_typing.py
@@ -1,17 +1,15 @@
-#course: Object-oriented programming, year 2, semester 1
-#academic year: 201819
-#author: B. Schoen-Phelan
-#date: 22-09-2018
-#purpose: Lab 1
+# course: Object-oriented programming, year 2, semester 1
+# academic year: 201819
+# author: B. Schoen-Phelan
+# date: 22-09-2018
+# purpose: Lab 1
 
 my_variable = 10
 while my_variable < 20:
-        i = my_variable
-        if i < 12:
-                my_variable +=1
-                print(my_variable)
-        else:
-                my_vairable = (my_variable + 20)
-                print('new')
-
-
+    i = my_variable
+    if i < 12:
+        my_variable += 1
+        print(my_variable)
+    else:
+        my_variable = (my_variable + 20)
+        print('new')
